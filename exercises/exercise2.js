a5855ce7e8613983c9e788cbf512aa9a4c386f2c
Damian Kowalski <dkowalski.1997@gmail.com>
2024-12-07 11:52:22 +0100

    generating address from public key

--- exercises/exercise2.js
+++ exercises/exercise2.js
@@ -1,7 +1,10 @@
-const { } = require('@bsv/sdk');
+const { PublicKey } = require('@bsv/sdk');
 
 /*
     Get PublicKey from String (hex encoded DER format), and
     - print its address
     - check it's the same as in the previous exercise
  */
+
+const pubKey = PublicKey.fromString('03752669a71d34c89e47cdeafe9d3cb5edec8e5a4d855ee487a7a0a6d266bde976');
+console.log("Address: ", pubKey.toAddress());
