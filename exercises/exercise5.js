d0b80620c733727a4e7eaec1cbf89dd57bb936e8
Damian Kowalski <dkowalski.1997@gmail.com>
2024-12-07 15:21:02 +0100

    excercise 5

--- exercises/exercise5.js
+++ exercises/exercise5.js
@@ -1,4 +1,4 @@
-const { } = require('@bsv/sdk');
+const { P2PKH } = require('@bsv/sdk');
 
 /*
   Generate locking script for a P2PKH address
@@ -7,7 +7,8 @@ const { } = require('@bsv/sdk');
   - remember the index of that output for the next exercise (as UTXO index)
  */
 
-const address = "put_your_address_here"
+const address = "1AWiBe2v3fSvVY6LQutEfqjR7m6qGbUsQu"
 
-const lockingScript = "" // <- TODO: update this line
+const p2 = new P2PKH();
+const lockingScript = p2.lock(address).toASM();
 console.log("Locking script: ", lockingScript)
--- exercises/save-key.js
+++ exercises/save-key.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const saveKey = (privKey) => {
     console.log('Saving wif format.')
 
-    fs.appendFile('private-keys.txt', `Created: ${new Date().toISOString()}\n` + `Private WIF: ${privKey.toWif()}` + '\n' + `Address: ${privKey.toAddress()}` + '\n' + `Public key: ${privKey.toPublicKey()}\n\n`, function (err) {
+    fs.appendFile('private-key-another.txt', `Created: ${new Date().toISOString()}\n` + `Private WIF: ${privKey.toWif()}` + '\n' + `Address: ${privKey.toAddress()}` + '\n' + `Public key: ${privKey.toPublicKey()}\n\n`, function (err) {
         if (err) throw err;
         console.log('Saved!');
       });
